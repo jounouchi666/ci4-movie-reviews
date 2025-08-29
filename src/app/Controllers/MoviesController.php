844b05fcbf79cefb2990763966ed9a31e6cdaaf7
jounouchi666 <trueguy666@gmail.com>
2025-08-29 22:15:44 +0900

    storeを構築

--- src/app/Controllers/MoviesController.php
+++ src/app/Controllers/MoviesController.php
@@ -2,10 +2,32 @@
 
 namespace App\Controllers;
 
+use App\Models\MovieModel;
+use CodeIgniter\HTTP\RedirectResponse;
+
 /**
  * Moviesコントローラー
  */
 class Movies extends BaseController
-{
+{    
+    /**
+     * 保存
+     *
+     * @return RedirectResponse リダイレクト
+     */
+    public function store(): RedirectResponse
+    {
+        // バリデーション
+        if (! $this->validate('movie')) {
+            $error = $this->validator->getErrors();
+            return redirect()->back()->withInput()->with('error', $error);
+        };
 
+        // 保存
+        $data = $this->request->getPost();
+        $model = model(MovieModel::class);
+        $model->insert($data);
+
+        return redirect()->to('/movies')->with('message', '登録しました');
+    }
 }
