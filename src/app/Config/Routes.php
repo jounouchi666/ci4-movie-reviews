1d7acaa03962d3fd67b416c77cc6855b14658efb
jounouchi666 <trueguy666@gmail.com>
2025-09-04 02:15:03 +0900

    ルート構築

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->group('movies', function($routes) {
+    $routes->get('', 'Movies::index', ['as' => 'index']);
+    $routes->get('show/{id}', 'Movies::show', ['as' => 'show']);
+    $routes->get('edit/{id?}', 'Movies::edit', ['as' => 'edit']);
+    $routes->post('save', 'Movies::save', ['as' => 'save']);
+    $routes->post('delete/{id}', 'Movies::delete', ['as' => 'delete']);
+});
