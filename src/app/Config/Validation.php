6e37c346f7667609fe828ff4445900660a6b4e01
jounouchi666 <trueguy666@gmail.com>
2025-09-11 17:50:34 +0900

    一覧の絞り込みフォーム用バリデーションを構築

--- src/app/Config/Validation.php
+++ src/app/Config/Validation.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+use App\Validation\CustomRules;
 use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Validation\StrictRules\CreditCardRules;
 use CodeIgniter\Validation\StrictRules\FileRules;
@@ -25,6 +26,7 @@ class Validation extends BaseConfig
         FormatRules::class,
         FileRules::class,
         CreditCardRules::class,
+        CustomRules::class,
     ];
 
     /**
@@ -42,11 +44,24 @@ class Validation extends BaseConfig
     // Rules
     // --------------------------------------------------------------------
 
+    // editフォームなど（moviesテーブルへのsave時に使用）
     public $movie = [
         'title' => 'required|min_length[1]|max_length[255]',
         'year' => 'required|integer|greater_than_equal_to[1900]',
         'genre' => 'required',
         'rating' => 'required|integer|greater_than_equal_to[0]|less_than_equal_to[5]',
         'review' => 'permit_empty|max_length[2000]',
+    ];
+
+    // フィルター用
+    public $movieFilter = [
+        'title' => 'permit_empty|max_length[255]',
+        'year_exact' => 'permit_empty|integer|greater_than_equal_to[1900]',
+        'year_min' => 'permit_empty|integer|greater_than_equal_to[1900]|checkRange[year_max]',
+        'year_max' => 'permit_empty|integer|greater_than_equal_to[1900]',
+        'genre' => 'permit_empty|max_length[100]',
+        'rating_exact' => 'permit_empty|integer|greater_than_equal_to[0]|less_than_equal_to[5]',
+        'rating_min' => 'permit_empty|integer|greater_than_equal_to[0]|less_than_equal_to[5]|checkRange[rating_max]',
+        'rating_max' => 'permit_empty|integer|greater_than_equal_to[0]|less_than_equal_to[5]',
     ];
 }
