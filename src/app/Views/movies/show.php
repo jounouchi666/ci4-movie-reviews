c17e2c09fd96d192113fb515092fc40747a67397
jounouchi666 <trueguy666@gmail.com>
2025-09-11 15:10:54 +0900

    修正・削除ボタン追加 リンクを名前付きルート仕様に修正

--- src/app/Views/movies/show.php
+++ src/app/Views/movies/show.php
@@ -17,6 +17,10 @@
     </li>
 
     <div class="nav">
-        <a href="<?= site_url(QueryHelper::buildUrl('movies/' , $filters)) ?>">一覧に戻る</a>
+        <a href="<?= site_url(QueryHelper::buildUrl(route_to('index'), $filters)) ?>">一覧に戻る</a>
+        <a href="<?= site_url(QueryHelper::buildUrl(route_to('edit', $movie['id']), $filters)) ?>">修正</a>
+        <form action=<?= QueryHelper::buildUrl(route_to('delete', $movie['id']), $filters) ?> method="post">
+            <input type="submit" value="削除">
+        </form>
     </div>
 </main>
