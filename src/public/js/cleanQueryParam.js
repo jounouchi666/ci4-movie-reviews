2ab973c621bedaed59b39c1089e4e15fd68072f9
jounouchi666 <trueguy666@gmail.com>
2025-09-30 19:01:09 +0900

    表示切替ラジオボタンを切り替え対象inputに応じて送信しないように設定

--- src/public/js/cleanQueryParam.js
+++ src/public/js/cleanQueryParam.js
@@ -13,11 +13,65 @@ export function cleanQueryParam(e) {
 
     // 値が空のパラメータを削除
     for (const [key, value] of formData.entries()) {
-        if (value !== '') params.append(key, value);
+        const input = form.querySelector(`[name='${CSS.escape(key)}']`);
+        if (!input) continue;
+
+        if (input.type === 'radio' && input.classList.contains('toggle-input')) {
+            if (hasValueToggleVisibilityRadio(key)) {
+                params.append(key, value);
+            }
+        } else {
+            if (value !== '') {
+                params.append(key, value);
+            }
+        }
     }
 
     const action = form.getAttribute('action') || location.pathname;
     const queryStr = params.toString();
 
     window.location.href = action + (queryStr ? '?' + queryStr : '');
+}
+
+
+/**
+ * 指定されたnameを持つ .toggle-input ラジオが選択されていて、
+ * そのターゲットに入力値がある場合のみ true を返す
+ *
+ * @param {string} key ラジオのname
+ * @returns {boolean}
+ */
+function hasValueToggleVisibilityRadio(key) {
+    const checked = document.querySelectorAll(`.toggle-input[name='${CSS.escape(key)}']:checked`);
+    if (checked.length === 0) return false;
+
+    return Array.from(checked).some(radio => {
+        if (!radio.dataset.toggleTarget) return false;
+
+        const targets = document.querySelectorAll(radio.dataset.toggleTarget);
+        return Array.from(targets).some(hasValueInTarget);
+    })
+}
+
+
+/**
+ * 対象要素内に値が入っているか判定
+ *
+ * @param {HTMLElement} target
+ * @returns {boolean}
+ */
+function hasValueInTarget(target) {   
+    return (
+        // input
+        Array.from(target.getElementsByTagName('input')).some(input =>
+            (['text', 'number'].includes(input.type) && input.value.trim() !== '') ||
+            (['checkbox', 'radio'].includes(input.type) && input.checked)
+        ) ||
+
+        // textarea
+        Array.from(target.getElementsByTagName('textarea')).some(textarea => textarea.value.trim() !== '') ||
+
+        // セレクトボックス
+        Array.from(target.getElementsByTagName('select')).some(select => select.value !== '')
+    );
 }
