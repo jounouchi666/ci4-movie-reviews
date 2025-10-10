6cc219da6ca89a1be7e4c1f98e06471af8977a53
jounouchi666 <trueguy666@gmail.com>
2025-10-10 15:25:19 +0900

    不要な属性を削除

--- src/app/Views/templates/layout.php
+++ src/app/Views/templates/layout.php
@@ -7,7 +7,7 @@
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- CSS -->
     <?= $this->renderSection('pageStyles') ?>
