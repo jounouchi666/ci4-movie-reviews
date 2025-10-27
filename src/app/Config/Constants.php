5b171ab9bd404bbc59c406422ab8f9d6391b6e5a
jounouchi666 <trueguy666@gmail.com>
2025-10-27 11:50:30 +0900

    ユーザーデフォルトアイコンパス追加

--- src/app/Config/Constants.php
+++ src/app/Config/Constants.php
@@ -81,4 +81,5 @@ defined('EXIT__AUTO_MAX')      || define('EXIT__AUTO_MAX', 125);    // highest a
 /**
  * ユーザープロフィールアイコンのディレクトリ
  */
-defined('USER_ICON_DIR') || define('USER_ICON_DIR', 'uploads/user_icons/');
+defined('DEFAULT_USER_ICON_PATH') || define('DEFAULT_USER_ICON_PATH', 'img/default_user_icon.svg'); // デフォルトユーザーアイコン
+defined('USER_ICON_DIR') || define('USER_ICON_DIR', 'uploads/user_icons/');                         // ユーザーアイコンのディレクトリ
