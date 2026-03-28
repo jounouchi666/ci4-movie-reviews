7d5b9e9b959bf91d57aa6c099c7a60f0fcc8a29d
jounouchi666 <trueguy666@gmail.com>
2026-03-29 00:29:07 +0900

    JsonSerializable追加

--- src/app/DTO/MovieGenreResultDTO.php
+++ src/app/DTO/MovieGenreResultDTO.php
@@ -2,10 +2,20 @@
 
 namespace App\DTO;
 
-final class MovieGenreResultDTO
+use JsonSerializable;
+
+final class MovieGenreResultDTO implements JsonSerializable
 {
     public function __construct(
         public int $id,
         public string $name
     ) {}
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name
+        ];
+    }
 }
