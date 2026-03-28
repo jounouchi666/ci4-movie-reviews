5fb6c8e46a83182e0c0fb763608fd500ef2c5332
jounouchi666 <trueguy666@gmail.com>
2026-03-29 02:23:44 +0900

    searchメソッドの構築

--- src/app/Controllers/Api/MovieSearch.php
+++ src/app/Controllers/Api/MovieSearch.php
@@ -2,18 +2,41 @@
 
 namespace App\Controllers\Api;
 
+use App\UseCases\MovieSearchUseCase;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\RESTful\ResourceController;
+use Config\Services;
+
 /**
  * MoviesSearchコントローラー
  */
-class MovieSearch extends \App\Controllers\BaseController
-{    
+class MovieSearch extends ResourceController
+{
+    private MovieSearchUseCase $movieSearchUseCase;
+
+    public function __construct()
+    {
+        $this->movieSearchUseCase = Services::movieSearchUseCase();
+    }
+
     /**
      * 検索
      *
-     * @return string JSON
+     * @return ResponseInterface
      */
-    public function search()
+    public function search(): ResponseInterface
     {
-        $query = $this->request->getGet('query');
+        /** @var \CodeIgniter\HTTP\IncomingRequest $request */
+        $request = $this->request;
+        $query = $request->getGet();
+
+        if (!$this->validateData($query, 'searchMovies')) {
+            $errors = $this->validator->getErrors();
+            return $this->failValidationErrors($errors);
+        }
+
+        $data = $this->movieSearchUseCase->execute($query['title']);
+        
+        return $this->respond($data, 200);
     }
 }
