5022e095e2188023380ef2f6d137c4b936b15417
jounouchi666 <trueguy666@gmail.com>
2026-03-29 16:25:05 +0900

    searchMovie作成

---
+++ src/public/js/api/searchMovie.js
@@ -0,0 +1,17 @@
+import { apiClient, SEARCH_URL } from "./apiClient";
+
+/**
+ * 映画を検索
+ * @param {string} title - 検索したい映画のタイトル
+ * @param {number} [page=1] - 検索結果の一覧ページ番号
+ * @returns {Promise<object>} 検索結果
+ */
+const createSearchMovie = url => async (title, page = 1) => {
+    const query = {
+        title,
+        page
+    }
+    return await apiClient.get(url, { query });
+}
+
+export const searchMovie = createSearchMovie(SEARCH_URL);
