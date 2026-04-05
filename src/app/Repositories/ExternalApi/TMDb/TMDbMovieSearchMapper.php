e9a855fe2f17f2a90d090e3add531d24342eb354
jounouchi666 <trueguy666@gmail.com>
2026-04-05 18:36:55 +0900

    DTOの生成を分離

--- src/app/Repositories/ExternalApi/TMDb/TMDbMovieSearchMapper.php
+++ src/app/Repositories/ExternalApi/TMDb/TMDbMovieSearchMapper.php
@@ -2,43 +2,19 @@
 
 namespace App\Repositories\ExternalApi\TMDb;
 
-use App\DTO\MovieGenreResultDTO;
-use App\DTO\MovieSearchItemResultDTO;
 use App\DTO\MovieSearchResultDTO;
 
 class TMDbMovieSearchMapper
 {
+    public function __construct(
+        private TMDbMovieSearchItemMapper $tmdbMovieSearchItemMapper,
+    ) {}
+
     public function toDTO(array $response, array $genreMap): MovieSearchResultDTO
     {
-        $imageBaseUrl = Config('TMDb')->imageBaseUrl;
-
-        $movies = array_map(fn ($movie) => new MovieSearchItemResultDTO(
-                id:             $movie['id'] ?? '',
-                title:          $movie['title'] ?? '',
-                originalTitle:  $movie['original_title'] ?? '',
-                genre:          array_map(fn($genreId) => new MovieGenreResultDTO(
-                                        id:     $genreId,
-                                        name:   $genreMap[$genreId] ?? ''
-                                    ),
-                                    $movie['genre_ids'] ?? []
-                                ),
-                overview:       $movie['overview'] ?? '',
-                posterPath:     !empty($movie['poster_path'])
-                                    ? "{$imageBaseUrl}/original/{$movie['poster_path']}"
-                                    : null,
-                posterUrl:      !empty($movie['poster_path'])
-                                    ? "{$imageBaseUrl}/original/{$movie['poster_path']}"
-                                    : base_url(DEFAULT_POSTER_IMAGE),
-                backdropPath:   !empty($movie['backdrop_path'])
-                                    ? "{$imageBaseUrl}/original/{$movie['backdrop_path']}"
-                                    : null,
-                adult:          $movie['adult'] ?? false,
-                releaseDate:    $movie['release_date'] ?? ''
-        ), $response['results'] ?? []);
-        
         return new MovieSearchResultDTO(
             page:           $response['page'] ?? 1,
-            results:        $movies,
+            results:        $this->tmdbMovieSearchItemMapper->mapFromGenreIdsFromArray($response['results'] ?? [], $genreMap),
             totalPages:     $response['total_pages'] ?? 0,
             totalResults:   $response['total_results'] ?? 0
         );
